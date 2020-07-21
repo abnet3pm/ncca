2265f6bced222b10709faab4e2bb5ed6e85b4173
abnet3pm <68604631+abnet3pm@users.noreply.github.com>
2020-07-21 10:43:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<html lang="es">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Notaria Clara Carnero</title>
+    <META NAME="SKYPE_TOOLBAR" CONTENT="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8" />
+</head>
+<body class="login">
+
+    <section class="wrapper login__wrapper">
+        <section class="login__content">
+            <div class="login__header">
+                <img src="images/notaria-clara-carnero.png">
+            </div>
+            <div class="login__box">
+                <h2 class="login__title">Módulo Web <span>SISNOT NCCA</span></h2>
+                <div class="login__form">
+                    <form action="login1-8.php" method="post">
+                        <ul class="login__form__list">
+                            <li class="login__form__item">
+                                <label class="login__form__label label_user">Usuario</label>
+                                <input type="text" name="user" autocomplete="off">
+                            </li>
+                            <li class="login__form__item">
+                                <label class="login__form__label label_pass">Contraseña</label>
+                                <input type="password" name="pass" autocomplete="off">
+                            </li>
+                            <li class="login__form__item">
+                                <button type="submit" class="button main_button login__form__button">Ingresar</button>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+            <!--div class="login__footer">
+                <a href="#" class="login__footer__link">Regresar al Índice de Módulos</a>
+            </div-->
+        </section>
+    </section>
+
+    <footer class="site_footer">
+        <div class="site_footer__copyright">
+            <p>© Copyright 2019 Notaría Clara Carnero</p>
+        </div>
+        <div class="site_footer__by">
+            <p>Adaptado por Jose Ponce Morales <a href="#"></a></p>
+        </div>
+    </footer>
+
+    <script src="js/jquery.js" type="text/javascript"></script>
+
+</body>
+</html>
